3c66b6512f917b51f657a4e3de69f0d463581096
alex <alex@chainus.co.kr>
2018-08-21 17:56:20 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public');
+
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+module.exports = app;
